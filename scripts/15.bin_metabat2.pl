3b2f28268f6dcaca6d8688f8105d0016e7954188
fpusan <fpusan@gmail.com>
2020-04-30 14:09:28 +0200

    support for debian10

--- scripts/15.bin_metabat2.pl
+++ scripts/15.bin_metabat2.pl
@@ -110,7 +110,7 @@ close outfile1;
 
 	#-- Running metabat2
 
-my $command="$metabat_soft -t $numthreads -i $tempfasta -a $depthfile -o $dirbin/metabat2 --saveTNF saved_1500.tnf --saveDistance saved_1500.dist";
+my $command="$metabat_soft -t $numthreads -i $tempfasta -a $depthfile -o $dirbin/metabat2";
 print outsyslog "Running metabat2 : $command\n";
 print "  Running metabat2 (Kang et al 2019, PeerJ 7, e7359)\n";
 my $ecode = system $command;
